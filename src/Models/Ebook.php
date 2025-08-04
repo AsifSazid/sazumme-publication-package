92c83eeb5abffc2a84f96a05e3a4424933f238a9
Asif Mostofa Sazid <asif83sazid@gmail.com>
2025-08-04 19:50:31 +0600

    version 2.0.1.1

--- src/Models/Ebook.php
+++ src/Models/Ebook.php
@@ -2,6 +2,11 @@
 
 namespace SazUmme\Publication\Models;
 
+use App\Models\Category;
+use App\Models\EbookPage;
+use App\Models\Image;
+use App\Models\Tag;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
@@ -14,13 +19,28 @@ class Ebook extends Model
 
     protected $guarded = [];
 
-    public function user()
-    {
-        return $this->belongsTo(User::class, 'written_by');
-    }
-
     public function image()
     {
         return $this->morphOne(Image::class, 'imageable');
+    }
+
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function tag()
+    {
+        return $this->belongsTo(Tag::class);
+    }
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class, 'category_ebook');
+    }
+
+    public function pages()
+    {
+        return $this->hasMany(EbookPage::class);
     }
 }
